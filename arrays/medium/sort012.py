c8c9a65c4e1f1ee0fc9a619fd057f0a28619e054
AdrielJG <135176227+AdrielJG@users.noreply.github.com>
2026-07-27 12:52:41 +0530

    Create sort012.py

---
+++ arrays/medium/sort012.py
@@ -0,0 +1,30 @@
+nums = [1, 0, 2, 1, 0]
+
+count0 = count1 = count2 = 0
+
+for i in nums:
+    if i == 0:
+        count0 += 1
+    elif i == 1:
+        count1 += 1
+    else:
+        count2 += 1
+
+i = 0
+
+while count0:
+    nums[i] = 0
+    i += 1
+    count0 -= 1
+
+while count1:
+    nums[i] = 1
+    i += 1
+    count1 -= 1
+
+while count2:
+    nums[i] = 2
+    i += 1
+    count2 -= 1
+
+print(nums)
