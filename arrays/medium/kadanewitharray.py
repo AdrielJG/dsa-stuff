811884361084e6bfa09e63f2bba17d3860da5f32
AdrielJG <135176227+AdrielJG@users.noreply.github.com>
2026-07-29 16:01:07 +0530

    Update kadanewitharray.py

--- arrays/medium/kadanewitharray.py
+++ arrays/medium/kadanewitharray.py
@@ -1,1 +1,22 @@
+nums = [2, 3, 5, -2, 7, -4]
+maxsum = nums[0]
+sums = nums[0]
 
+start = 0
+end = 0
+temp_start = 0
+
+for i in range(1, len(nums)):
+    if nums[i] > sums + nums[i]:
+        sums = nums[i]
+        temp_start = i
+    else:
+        sums += nums[i]
+    
+    if sums > maxsum:
+        maxsum = sums
+        start = temp_start
+        end = i
+
+print(maxsum)
+print(nums[start:end+1])
