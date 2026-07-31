fe05a2ec963175078b5be361aafbddd47c29af9f
AdrielJG <135176227+AdrielJG@users.noreply.github.com>
2026-08-01 01:56:33 +0530

    Update positivenegative.py

--- arrays/medium/positivenegative.py
+++ arrays/medium/positivenegative.py
@@ -1,1 +1,13 @@
+arr = [2, 4, 5, -1, -3, -4]
+yes = [0] * len(arr)
+pos, neg = 0, 1
 
+for i in arr:
+    if i > 0:
+        yes[pos] = i
+        pos += 2
+    else:
+        yes[neg] = i
+        neg += 2
+
+print(yes)
